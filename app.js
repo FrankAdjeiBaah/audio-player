4f7d2018962281a9540665e1da875248c430dfd1
dbsimeonov <dbsimeonov@yahoo.com>
2018-05-24 16:31:20 +0100

    css finished, js bugs

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// 
+// Creating the Markup
+// 
+
 // Decalaring variables
 const UIplayer = document.querySelector('.player');
 
-// Creating the markup
 // Background parent div
 let background = createEl('div');
 background.id = 'bg';
@@ -40,7 +43,8 @@ main.appendChild(mainPlayer);
 let songTitle = createEl('div');
 songTitle.id = 'songTitle';
 // For testing and visual
-songTitle.innerHTML  = 'WE ROCK FOREEVEr'
+songTitle.innerHTML  = 'WE ROCK FOREEVEr';
+
 mainPlayer.appendChild(songTitle);
 
 // Buttons Container
@@ -50,16 +54,16 @@ mainPlayer.appendChild(buttons);
 
 // Inside buttons 
 let pre = createEl('button');
-let next = createEl('button');
 let play = createEl('button');
+let next = createEl('button');
 
 pre.id = 'pre';
 next.id = 'next';
 play.id = 'play';
 
 buttons.appendChild(pre);
-buttons.appendChild(next);
 buttons.appendChild(play);
+buttons.appendChild(next);
 
 // Inside each button there is an image
 let preImg = createEl('img');
@@ -86,79 +90,92 @@ seekbar.id = 'seek-bar';
 player.appendChild(seekbar);
 
 // Inside seekbar
-let fill = createEl('div');
+let fillBar = createEl('div');
 let handle = createEl('div');
-fill.id = 'fill';
+fillBar.id = 'fill';
 handle.id = 'handle';
 
-seekbar.appendChild(fill);
+seekbar.appendChild(fillBar);
 seekbar.appendChild(handle);
-
-
-
 
 // Function for creating Elements
 function createEl(text){
     return document.createElement(text);
 }
+// 
+//  Functionality
+// 
 
 
-// const songs = ["music/Blues Saraceno  Dogs of War.mp3","music/Blues Saraceno - Devils Got You Beat.mp3","music/Blues Saraceno - Outlaw Justice.mp3"];
-// const poster = ["images/poster1.jpg","images/poster2.jpg","images/poster3.jpg"];
-// const songTitle = document.getElementById("songTitle");
-// const fillBar = document.getElementById("fill");
-// let time = document.getElementById('time');
-// let song = new Audio();
-// let currentSong = 0;    // it point to the current song
-// window.onload = playSong;   // it will call the function playSong when window is load
 
-// function playSong(){
-//     song.src = songs[currentSong];  //set the source of 0th song 
-//     songTitle.textContent = songs[currentSong].slice(6, -4); // set the title of song
-//     song.play();    // play the song
-// }
+// Declaring Variables
 
-// function playOrPauseSong(){
-//     if(song.paused){
-//         song.play();
-//         $("#play img").attr("src","images/Pause.png");
-//     }
-//     else{
-//         song.pause();
-//         $("#play img").attr("src","images/Play.png");
-//     }
-// }
+const songs = ["music/Blues Saraceno  Dogs of War.mp3","music/Blues Saraceno - Devils Got You Beat.mp3","music/Blues Saraceno - Outlaw Justice.mp3"];
+const poster = ["images/poster1.jpg","images/poster2.jpg","images/poster3.jpg"];
+let song = new Audio();
+let currentSong = 0;    // it point to the current song
+// Might remove it to be paused on load
+window.onload = playSong;   // it will call the function playSong when window is load
 
-// song.addEventListener('timeupdate',function(){ 
-//     var position = song.currentTime / song.duration;
-//     // console.log(Math.round(song.currentTime), song.duration)
-//     time.innerHTML = Math.round(song.currentTime) + `s / ${Math.round(song.duration)}s`;
-//     fillBar.style.width = position * 100 +'%';
+pre.addEventListener('click', preSong());
+next.addEventListener('click', nextSong());
+play.addEventListener('click', playOrPauseSong());
 
-//     if(song.currentTime == song.duration){
-//         next();
-//     }
-// });
+// Play a song with currentIndex
+function playSong(){
+    song.src = songs[currentSong];  //set the source of 0th song 
+    songTitle.textContent = songs[currentSong].slice(6, -4); // set the title of song
+    song.play();    // play the song
+}
 
+// Function for Play/Pause a song
+function playOrPauseSong(){
+    if(song.paused){
+        song.play();
+        $("#play img").attr("src","images/Pause.png");
+    }
+    else{
+        song.pause();
+        $("#play img").attr("src","images/Play.png");
+    }
+}
+// Event listener for timeupdate
+song.addEventListener('timeupdate',function(){ 
+    // Declaring the variable on each timeupdate
+    var position = song.currentTime / song.duration;
+    // Updating the seconds/time container
+    time.innerHTML = Math.round(song.currentTime) + `s / ${Math.round(song.duration)}s`;
+    // Updating the width from the currentTime
+    fillBar.style.width = position * 100 +'%';
+    // If song is finished, play next
+    if(song.currentTime == song.duration){
+        nextSong();
+    }
+});
 
-// function next(){
-//     currentSong++;
-//     if(currentSong > 2){
-//         currentSong = 0;
-//     }
-//     playSong();
-//     $("#play img").attr("src","images/Pause.png");
-//     $("#image img").attr("src",poster[currentSong]);
-//     $("#bg img").attr("src",poster[currentSong]);
-// }
-
-// function pre(){
-//     currentSong--;
-//     if(currentSong < 0){
-//         currentSong = 2;
-//     }
-//     playSong();
-//     $("#play img").attr("src","images/Pause.png");
-//     $("#image img").attr("src",poster[currentSong]);
-//     $("#bg img").attr("src",poster[currentSong]);
-// }
+// nextsong
+function nextSong(){
+    // next song add 1 on current song var
+    currentSong++;
+    // Larger than 2, go back to 0(3 songs in the array)
+    if(currentSong > 2){
+        currentSong = 0;
+    }
+    // Start the song
+    playSong();
+    // Assign images
+    $("#play img").attr("src","images/Pause.png");
+    $("#image img").attr("src",poster[currentSong]);
+    $("#bg img").attr("src",poster[currentSong]);
+}
+// previous song
+function preSong(){
+    currentSong--;
+    if(currentSong < 0){
+        currentSong = 2;
+    }
+    playSong();
+    $("#play img").attr("src","images/Pause.png");
+    $("#image img").attr("src",poster[currentSong]);
+    $("#bg img").attr("src",poster[currentSong]);
+}
